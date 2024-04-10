0bc3f87c10f1ac6f7d4e6d20bad119db8ad6b02d
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-04-10 14:28:22 +0300

    added postprocess.py

---
+++ source/scripts/postprocess.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+from pathlib import Path
+
+root = Path(sys.argv[1])
+external = root / "externals" / "chuck~.mxo" / "Contents"
+resources = external / "Resources"
+examples = root / "examples"
+external_examples = resources / "examples"
+
+
+print('-'*80)
+
+print(f"project root: {root}")
+if not resources.exists():
+	resources.mkdir()
+	print(f"created: {resources}")
+else:
+	print("skipped creating Resources folder")
+
+if not external_examples.exists():
+	shutil.copytree(examples, external_examples)
+	print(f"copied {examples} to {external_examples}")
+else:
+	print(f"skipped copying {examples} to {external_examples}")
+
+print('-'*80)
+
