e84750d8640f78bfb3f21e3c55ca6e6ba3556054
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-08-12 18:42:46 +0200

    fixes for windows

--- source/scripts/postprocess.py
+++ source/scripts/postprocess.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 
-import os
 import sys
-import shutil
+import platform
 from pathlib import Path
 
+PLATFORM = platform.system()
+
 root = Path(sys.argv[1])
-external = root / "externals" / "chuck~.mxo" / "Contents"
-resources = external / "Resources"
-examples = root / "examples"
-external_examples = resources / "examples"
+
+if PLATFORM == "Darwin":
+	external = root / "externals" / f"chuck~.mxo" / "Contents"
+	resources = external / "Resources"
+	examples = root / "examples"
+	external_examples = resources / "examples"
 
 
-print('-'*80)
+	print('-'*80)
 
-print(f"project root: {root}")
-if not resources.exists():
-	resources.mkdir()
-	print(f"created: {resources}")
-else:
-	print("skipped creating Resources folder")
+	print(f"project root: {root}")
+	if not resources.exists():
+		resources.mkdir()
+		print(f"created: {resources}")
+	else:
+		print("skipped creating Resources folder")
 
-# if not external_examples.exists():
-# 	shutil.copytree(examples, external_examples)
-# 	print(f"copied {examples} to {external_examples}")
-# else:
-# 	print(f"skipped copying {examples} to {external_examples}")
+	# if not external_examples.exists():
+	# 	shutil.copytree(examples, external_examples)
+	# 	print(f"copied {examples} to {external_examples}")
+	# else:
+	# 	print(f"skipped copying {examples} to {external_examples}")
 
-print('-'*80)
+	print('-'*80)
 
